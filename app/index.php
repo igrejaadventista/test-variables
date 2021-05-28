ed924e8244598ca31205c849dcee58c5480763c0
Eli Mendonca <contato@elimendonca.com>
2021-05-28 08:21:04 -0300

    Tentativa dos secrets

--- app/index.php
+++ app/index.php
@@ -1,4 +1,4 @@
-<iframe src="https://app-iaja.sdasystems.org/Pagppg/Login.aspx"></iframe>
+<iframe src="https://app-iaja.sdasystems.org/Pagppg/Login.aspx" width="100%" height="300" style="border:none;"></iframe>
 
 <?php
 
